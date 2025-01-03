7600135d77057ea484ac2e0756b8ce772f578679
unknown <blandinedjoumsie@gmail.com>
2025-01-03 14:13:08 -0600

    created a custom node js server, then handled a request and created a custom response.

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-let username = "julienne";
-console.log (username);
+//let username = "julienne";
+//console.log (username);
+const http = require('http');
+
+function handleRequest(request, response) {
+    response.statusCode = 200;
+    response.end('<h1>Hello World!<h1>');
+}
+
+const server = http.createServer(handleRequest);
+
+server.listen(3000);
+//amazon.com => Send a request to Amazon's server
+// amazon.com:443 or 80
