fef03f7d973db2d16ce3b8ea6aed07aaf04a5032
unknown <blandinedjoumsie@gmail.com>
2025-01-15 06:55:28 -0600

    added some code which reads data stored in my JSON file and then generates dynamycally some HTML content

--- app.js
+++ app.js
@@ -20,8 +20,9 @@ server.get('/', function(req, res){
 
 server.post('/store-user', function(req, res){
     const userName= req.body.username;
-    const filePath = path.join(__dirname, 'data', 'users.json')
+    const filePath = path.join(__dirname, 'data', 'users.json');
     
+    //the sync method performs the operation instantly
     const fileData = fs.readFileSync(filePath);
     const existingUsers = JSON.parse(fileData);
     //JSON.parse transforms raw text from the JSON file into javascript object or array
@@ -33,6 +34,21 @@ server.post('/store-user', function(req, res){
     fs.writeFileSync(filePath, JSON.stringify(existingUsers));
     res.send('<h1>Username stored!</h1>');
 });
+
+server.get('/users', function(req, res){
+    const filePath = path.join(__dirname, 'data', 'users.json');
+    const fileData = fs.readFileSync(filePath);
+    const existingUsers = JSON.parse(fileData);
+
+    let responseData = '<ul>'
+    for (user of existingUsers){
+        responseData +=  '<li>' + user + '</li>'
+    }
+    responseData += '</ul>'
+
+    res.send(responseData)
+
+})
 
 server.listen(3000)
 
